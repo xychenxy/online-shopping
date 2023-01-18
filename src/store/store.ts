91614b73b1bb44478d804778d89155df7212a819
xychenxy <mavischen916@gmail.com>
2023-01-19 10:46:50 +1100

    update createStore to configStore as createStore is deprecated

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { compose, createStore, applyMiddleware, Middleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
@@ -13,26 +13,10 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger];
-const composeEnhancers = compose(applyMiddleware(...middleWares));
-
-export const store = createStore(persistedReducer, undefined, composeEnhancers);
+export const store = configureStore({
+	reducer: persistedReducer,
+	// devTools: process.env.NODE_ENV !== "production",
+	middleware: [logger],
+});
 
 export const persistor = persistStore(store);
-
-// import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// import { rootReducer } from "./root-reducer";
-
-// const persistConfig = {
-// 	key: "root",
-// 	storage,
-// 	blacklist: ["user"],
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-// export const store = configureStore({
-// 	reducer: persistedReducer,
-// });
-// export const persistor = persistStore(store);
