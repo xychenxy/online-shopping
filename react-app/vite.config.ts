0f5bf1ddcfe25828f245f031354ff3ceade124bb
xychenxy <mavischen916@gmail.com>
2023-02-14 14:31:05 +1100

    wrap backend and react-app into docker container

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -6,4 +6,8 @@ import macrosPlugin from "vite-plugin-babel-macros";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr(), macrosPlugin()],
+	server: {
+		host: true,
+		port: 8080,
+	},
 });
